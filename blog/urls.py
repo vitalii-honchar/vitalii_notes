805d9d65b1ae16efd52b00fafb1ec4e88fe2cbc0
Vitalii Honchar <weaxme@gmail.com>
2022-04-15 09:04:46 +0200

    Added authorization

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('post/<int:pk>/', views.BlogPostView.as_view(), name='blog_post')
+    path('post/<int:pk>/', views.BlogPostView.as_view(), name='blog_post'),
+    path('login/', views.BlogLogin.as_view(), name='login')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,29 @@
 from django.views import generic
 from .models import BlogPost
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth import views as auth_views
 
 from .models import BlogPost
 
 
-class IndexView(generic.ListView):
-    template_name = "blog/index.html"
-    context_object_name = "blog_post_list"
+class IndexView(LoginRequiredMixin, PermissionRequiredMixin, generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'blog_post_list'
+    login_url = 'blog:login'
+    permission_required = 'blog.view_blogpost'
 
     def get_queryset(self):
         return BlogPost.objects.order_by('-published_date')
 
 
-class BlogPostView(generic.DetailView):
+class BlogPostView(LoginRequiredMixin, PermissionRequiredMixin, generic.DetailView):
     model = BlogPost
     template_name = 'blog/blog_post.html'
+    login_url = 'blog:login'
+    permission_required = 'blog.view_blogpost'
+
+
+class BlogLogin(auth_views.LoginView):
+    next_page = 'blog:index'
