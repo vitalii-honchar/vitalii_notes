f9b67f5b150f1c7fa21a269da6af669ca6506f38
Vitalii Honchar <weaxme@gmail.com>
2022-04-17 13:57:16 +0200

    Used postgresql

--- vitalii_notes/settings.py
+++ vitalii_notes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'vitalii_notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vitalii_notes',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
